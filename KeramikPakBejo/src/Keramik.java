bd0d064d31646e94b3dd52589283daf9302d7e50
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2020-09-13 18:23:02 +0700

    Add files via upload

---
+++ KeramikPakBejo/src/Keramik.java
@@ -0,0 +1,49 @@
+public class Keramik {
+    int panjang, lebar, isi, harga;
+    float totalKeramik, totalBox, totalHarga;
+
+    private void keramik(String x){
+        if (x.equals("A")){
+            panjang = 30;
+            lebar = 30;
+            isi = 10;
+            harga = 54000;
+        }
+        else if (x.equals("B")){
+            panjang = 40;
+            lebar = 40;
+            isi = 5;
+            harga = 65000;
+        }
+        else if (x.equals("C")){
+            panjang = 30;
+            lebar = 40;
+            isi = 8;
+            harga = 60000;
+        }
+        hitung();
+        System.out.println("Total Harga jika Menggunakan Keramik " + x +" adalah "  +(int) totalHarga);
+    }
+
+    void hitung(){
+        int luasTotal = 1000000;
+        totalKeramik = luasTotal/(panjang*lebar);
+        if (totalKeramik - Math.round(totalKeramik) != 0 && totalKeramik - Math.round(totalKeramik) < 0.5){
+            totalKeramik = Math.round(totalKeramik) + 1;
+        }
+        totalBox = totalKeramik/isi;
+        if (totalBox - Math.round(totalBox) != 0 && totalBox - Math.round(totalBox) < 0.5){
+            totalBox = Math.round(totalBox) + 1;
+        }
+        totalHarga = totalBox * harga;
+
+    }
+
+    public static void main(String[] args){
+        Keramik bejo = new Keramik();
+        bejo.keramik("A");
+        bejo.keramik("B");
+        bejo.keramik("C");
+
+    }
+}
