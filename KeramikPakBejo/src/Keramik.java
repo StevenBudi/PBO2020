68e87a8f6ecd708e0137d2c7cf39f510d5cea361
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2020-09-24 20:11:19 +0700

    Update Keramik.java

--- KeramikPakBejo/src/Keramik.java
+++ KeramikPakBejo/src/Keramik.java
@@ -4,22 +4,22 @@ public class Keramik {
 
     protected void keramik(String x){
         if (x.equals("A")){
-            panjang = 30;
-            lebar = 30;
-            isi = 10;
-            harga = 54000;
+            this.panjang = 30;
+            this.lebar = 30;
+            this.isi = 10;
+            this.harga = 54000;
         }
         else if (x.equals("B")){
-            panjang = 40;
-            lebar = 40;
-            isi = 5;
-            harga = 65000;
+            this.panjang = 40;
+            this.lebar = 40;
+            this.isi = 5;
+            this.harga = 65000;
         }
         else if (x.equals("C")) {
-            panjang = 30;
-            lebar = 40;
-            isi = 8;
-            harga = 60000;
+            this.panjang = 30;
+            this.lebar = 40;
+            this.isi = 8;
+            this.harga = 60000;
         }
         hitung();
         System.out.println("Total Harga jika Menggunakan Keramik " + x +" adalah "  +(int) totalHarga);
@@ -27,21 +27,21 @@ public class Keramik {
 
     void hitung(){
         int luasTotal = 1000000;
-        totalKeramik = luasTotal/(panjang*lebar);
+        totalKeramik = luasTotal/(this.panjang*this.lebar);
         if (totalKeramik - Math.round(totalKeramik) != 0 && totalKeramik - Math.round(totalKeramik) < 0.5){
             totalKeramik = Math.round(totalKeramik) + 1;
         }
         else {
             totalKeramik = Math.round(totalKeramik);
         }
-        totalBox = totalKeramik/isi;
+        totalBox = totalKeramik/this.isi;
         if (totalBox - Math.round(totalBox) != 0 && totalBox - Math.round(totalBox) < 0.5){
             totalBox = Math.round(totalBox) + 1;
         }
         else {
             totalBox = Math.round(totalBox);
         }
-        totalHarga = totalBox * harga;
+        totalHarga = totalBox * this.harga;
 
     }
 
